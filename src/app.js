332aed70ee53d13e13ab59c27a1dfcceddc175e5
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-09 03:10:38 +0530

    chore: set up initial Express application

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+require("dotenv").config()
+const { connectDB } = require("./database")
+const routes = require("./routes")
+
+const app = express()
+
+// middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// database connection
+connectDB()
+
+// routes
+app.get("/", (req, res) => {
+  res.send({ message: ` Task handler application` })
+})
+
+app.use("/", routes)
+
+// error handler
+// app.use((err, req, res, next) => {
+//   console.error(err.stack)
+//   res.sendStatus(500).send({ message: `Something went wrong!` })
+// })
+
+const PORT = process.env.PORT || 8383
+app.listen(PORT, () => {
+  console.log(`\nServer listening on port ${PORT} `)
+})
