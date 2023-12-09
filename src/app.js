79fb5831f3f342d24613e6edc6f2dc422e94364b
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-09 21:36:02 +0530

    refactor(db): Update database connection in app.js to use pool instead of client

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express")
-require("dotenv").config()
 const pool = require("./db")
 const routes = require("./routes")
+require("dotenv").config()
 
 const app = express()
 
