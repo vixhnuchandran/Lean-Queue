af61a7dc821c49fa46639f3d22b9b87d2f566d5c
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-11 19:00:40 +0530

    Refactor services into a class, apply error handling, and update routes accordingly

--- src/routes.js
+++ src/routes.js
@@ -1,36 +1,55 @@
 const router = require("express").Router()
-const { colorize } = require("./utils")
+const { logError, red, green, yellow, blue, magenta, cyan } = require("./utils")
+const Services = require("./services")
 const {
-  createQueues,
-  addTasks,
-  getNextTasks,
-  submitResults,
-} = require("./services")
+  areQueueParametersValid,
+  isQueueIdValid,
+  isQueueTypeValid,
+  areOptionsValid,
+  areAllTasksValid,
+} = require("./validations")
+const { QueueError, ValidationError } = require("./error")
 require("dotenv").config()
+const INTERNAL_SERVER_ERROR = "Internal server error"
 
 /**
  * Route  for handling POST request to create queue ✅
  */
 router.post("/create-queue", async (req, res) => {
-  const { type, tasks } = req.body
+  const { type, tasks, options = null } = req.body
 
+  // VALIDATION
   try {
     if (!type) {
-      return res.status(400).json({ error: `type not specified` })
+      throw new QueueError("Missing type")
+    } else if (!tasks) {
+      throw new QueueError("Missing tasks")
+    } else if (!areQueueParametersValid(type, tasks, options)) {
+      throw new ValidationError("Invalid tasks")
     }
-    if (!tasks) {
-      return res.status(400).json({ error: `tasks not specified` })
-    }
-
-    const id = await createQueues(type)
-    const numTasks = await addTasks(id, tasks)
-
-    return res.json({ queue: id, numTasks })
   } catch (error) {
-    console.error(
-      colorize(`Error in route for create-queue: ${error.message}`, "red")
+    if (err instanceof ValidationError) {
+      console.log("ValidationError: Invalid queue parameters")
+      return res.status(400).json({ error: err.message })
+    } else if (err instanceof QueueError) {
+      console.log("QueueError")
+      return res.status(400).json({ error: err.message })
+    } else {
+      console.log("Unknown Error: ", err.message)
+    }
+  }
+
+  // PROCESSING
+  try {
+    const { queue, numTasks } = await Services.createQueueAndAddTasks(
+      type,
+      options,
+      tasks
     )
-    return res.status(500).json({ error: "Internal server error" })
+    return res.json({ queue, numTasks })
+  } catch (err) {
+    console.log("Unknown Error: ", err.message)
+    return res.status(500).json({ error: INTERNAL_SERVER_ERROR })
   }
 })
 
@@ -38,77 +57,142 @@ router.post("/create-queue", async (req, res) => {
  * Route  for handling POST request to add task ✅
  */
 router.post("/add-tasks", async (req, res) => {
-  const { queue, tasks } = req.body
+  const { queue, tasks, options } = req.body
+
+  // VALIDATION
   try {
     if (!queue) {
-      res.status(400).json({ error: "Bad Request: Missing queue" })
+      throw new QueueError("Missing queue")
+    } else if (!tasks) {
+      throw new QueueError("Missing tasks")
+    } else if (queue && !isQueueIdValid(queue)) {
+      throw new ValidationError("Invalid queue")
+    } else if (tasks && !areAllTasksValid(tasks)) {
+      throw new ValidationError("Invalid tasks")
     }
-    if (!tasks) {
-      res.status(400).json({ error: "Bad Request: Missing tasks" })
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      console.log("ValidationError: Invalid queue parameters")
+      return res.status(400).json({ error: err.message })
+    } else if (err instanceof QueueError) {
+      console.log("QueueError")
+      return res.status(400).json({ error: err.message })
+    } else {
+      console.log("Unknown Error:", err)
     }
-    const numTasks = await addTasks(queue, tasks)
+  }
+
+  // PROCESSING
+  try {
+    const numTasks = await Services.addTasks(queue, tasks, options)
     return res.json({ numTasks })
-  } catch (error) {
-    console.error(
-      colorize(`Error in route for add-tasks: ${error.message}`, "red")
-    )
-    res.status(500).json({ error: "Internal server error" })
-    return
+  } catch (err) {
+    console.log("Unknown Error:", err)
+    return res.status(500).json({ error: INTERNAL_SERVER_ERROR })
   }
 })
 
 /**
- * Route  for handling POST request to get the next task ✅
+ * Route  for handling POST requestto get the next task
  */
-router.post("/get-available-tasks", async (req, res) => {
-  const queue = req.body
+router.post("/get-next-available-task", async (req, res) => {
+  const { queue, type } = req.body
 
-  if (!queue) {
-    res.status(400).json({ error: "Bad Request: Missing queue" })
+  try {
+    if (!queue && !type) {
+      throw new QueueError("Either queue or type must be specified")
+    } else if (queue && isQueueIdValid) {
+      throw new ValidationError("Invalid queueId")
+    } else if (type && !isQueueTypeValid) {
+      throw new ValidationError("Invalid queueType")
+    }
+  } catch {
+    if (err instanceof ValidationError) {
+      console.log("ValidationError: Invalid queue parameters")
+      return res.status(400).json({ error: err.message })
+    } else if (err instanceof QueueError) {
+      console.log("QueueError")
+      return res.status(400).json({ error: err.message })
+    } else {
+      console.log("Unknown Error:", err)
+    }
   }
 
   try {
-    const task = await getNextTasks(queue)
-    if (!task) {
-      return res.send(404)
+    let nextAvailableTask
+    if (queue) {
+      nextAvailableTask = await Services.getNextAvailableTaskByQueue(queue)
+    } else if (type) {
+      nextAvailableTask = await Services.getNextAvailableTaskByType(type)
     }
     return res.status(200).json({
-      id: task.id,
-      params: task.params,
+      id: nextAvailableTask.id,
+      params: nextAvailableTask.params,
     })
-  } catch (error) {
-    console.error(
-      colorize("Error in route for get-available-tasks: ", error.message),
-      "red"
-    )
-    return res.status(500).json({
-      error: error.message,
-    })
+  } catch (err) {
+    console.log("Unknown Error:", err)
+    return res.status(500).json({ error: INTERNAL_SERVER_ERROR })
   }
 })
 
 /**
- * Route  for handling POST request to submit the results ✅
+ * Route for handling GET request to get the results using queue id
+ */
+router.get("/get-results/:queue", async (req, res) => {
+  const queue = req.params.queue
+
+  try {
+    // check queue is valid
+    // check queue in db
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      console.log("ValidationError: Invalid queue parameters")
+      return res.status(400).json({ error: err.message })
+    } else if (err instanceof QueueError) {
+      console.log("QueueError")
+      return res.status(400).json({ error: err.message })
+    } else {
+      console.log("Unknown Error:", err)
+    }
+  }
+
+  try {
+    const response = await Services.getResults(queue)
+    console.log(response)
+    if (Object.keys(response.results).length === 0) {
+      return res.status(400).json({ message: "No completed tasks found" })
+    } else {
+      return res.status(200).json(response)
+    }
+  } catch (err) {
+    console.log("Unknown Error:", err)
+    return res.status(500).json({ error: INTERNAL_SERVER_ERROR })
+  }
+})
+
+/**
+ * Route  for handling POST request to submit the results
  */
 router.post("/submit-results", async (req, res) => {
   const { id, result, error } = req.body
 
   try {
-    const response = await submitResults({ id, result, error })
-
+    const response = await Services.submitResults({ id, result, error })
+    console.log(response)
     if (response) {
       console.log("All jobs finished")
     }
     res.send({ ok: true })
     return
-  } catch (error) {
-    console.error(
-      colorize("Error in route for get-available-tasks: ", error.stack),
-      "red"
-    )
+  } catch (err) {
+    console.log(red("Error in route for submit-results: ", err))
     res.status(500).send({ ok: false, error: "Internal Server Error" })
     return
   }
 })
 
 module.exports = router
+
+// add get esults ebndpoiunt tpo dend result
+
+// ma esqkl  file and useeit in iosde
