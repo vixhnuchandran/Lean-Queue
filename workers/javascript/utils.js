e0b9f0851376b9ef7a846f454a51747af67c891e
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-11 19:01:08 +0530

    worker util

--- workers/javascript/utils.js
+++ workers/javascript/utils.js
@@ -11,16 +11,16 @@ const colors = {
   cyan: "\x1b[36m",
 }
 
-const setColor = (text, color) => {
+const colorize = (text, color) => {
   return `${colors[color]}${text}${colors.reset}`
 }
 
-const red = text => setColor(text, "red")
-const green = text => setColor(text, "green")
-const yellow = text => setColor(text, "yellow")
-const blue = text => setColor(text, "blue")
-const magenta = text => setColor(text, "magenta")
-const cyan = text => setColor(text, "cyan")
+const red = text => colorize(text, "red")
+const green = text => colorize(text, "green")
+const yellow = text => colorize(text, "yellow")
+const blue = text => colorize(text, "blue")
+const magenta = text => colorize(text, "magenta")
+const cyan = text => colorize(text, "cyan")
 
 const getRandom = (min, max, seed = Date.now()) => {
   const rng = seedrandom(seed)
