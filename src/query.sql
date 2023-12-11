22eb7d9862295e08e9559e0463cf5e10f9396f2a
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-11 18:55:46 +0530

    Add SQL queries to create 'queues' and 'tasks' tables with associated types

---
+++ src/query.sql
@@ -0,0 +1,21 @@
+
+CREATE TABLE IF NOT EXISTS queues (
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(20) NOT NULL ,
+  options JSONB 
+);
+  
+CREATE TYPE task_status AS ENUM ('available', 'processing', 'completed', 'error');
+
+CREATE TABLE IF NOT EXISTS tasks (
+  id SERIAL PRIMARY KEY,
+  task_id VARCHAR(255) NOT NULL,
+  params JSONB NOT NULL,
+  status task_status DEFAULT 'available',
+  result JSONB DEFAULT NULL,
+  start_time TIMESTAMP DEFAULT NULL,
+  end_time TIMESTAMP DEFAULT NULL,
+  expiry_time TIMESTAMP DEFAULT NULL,
+  queue_id INTEGER REFERENCES queues(id)
+);
+
