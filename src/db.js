ca729519c6d34329f075409869dd6127b190cf03
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-12 02:55:53 +0530

    chore: Set max pool size to 5 in PostgreSQL configuration

--- src/db.js
+++ src/db.js
@@ -4,6 +4,7 @@ require("dotenv").config()
 const connectionString = process.env.POSTGRES_URL
 const pool = new Pool({
   connectionString,
+  max: 5,
 })
 
 pool.on("connect", () => {
@@ -14,4 +15,4 @@ pool.on("error", err => {
   console.error("Error connecting to the PostgreSQL database:", err.message)
 })
 
-module.exports = pool
+module.exports = { pool }
