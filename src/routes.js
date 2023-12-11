cb23980a6d8b89586a87655a8dd0fc1b3956bd64
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-11 19:31:04 +0530

    Refactor code to use custom logger for consistent and improved error handling

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,13 @@
 const router = require("express").Router()
-const { logError, red, green, yellow, blue, magenta, cyan } = require("./utils")
+const {
+  customLogger,
+  red,
+  green,
+  yellow,
+  blue,
+  magenta,
+  cyan,
+} = require("./utils")
 const Services = require("./services")
 const {
   areQueueParametersValid,
@@ -27,15 +35,17 @@ router.post("/create-queue", async (req, res) => {
     } else if (!areQueueParametersValid(type, tasks, options)) {
       throw new ValidationError("Invalid tasks")
     }
+
+    // Options Valid
   } catch (error) {
     if (err instanceof ValidationError) {
-      console.log("ValidationError: Invalid queue parameters")
+      customLogger("error", red, "ValidationError: Invalid queue parameters")
       return res.status(400).json({ error: err.message })
     } else if (err instanceof QueueError) {
-      console.log("QueueError")
+      customLogger("error", red, "QueueError")
       return res.status(400).json({ error: err.message })
     } else {
-      console.log("Unknown Error: ", err.message)
+      customLogger("error", red, "Unknown Error: ", err.message)
     }
   }
 
@@ -46,9 +56,10 @@ router.post("/create-queue", async (req, res) => {
       options,
       tasks
     )
+
     return res.json({ queue, numTasks })
   } catch (err) {
-    console.log("Unknown Error: ", err.message)
+    customLogger("error", red, "Unknown Error: ", err.message)
     return res.status(500).json({ error: INTERNAL_SERVER_ERROR })
   }
 })
@@ -72,13 +83,13 @@ router.post("/add-tasks", async (req, res) => {
     }
   } catch (err) {
     if (err instanceof ValidationError) {
-      console.log("ValidationError: Invalid queue parameters")
+      customLogger("error", red, "ValidationError: Invalid queue parameters")
       return res.status(400).json({ error: err.message })
     } else if (err instanceof QueueError) {
-      console.log("QueueError")
+      customLogger("error", red, "QueueError")
       return res.status(400).json({ error: err.message })
     } else {
-      console.log("Unknown Error:", err)
+      customLogger("error", red, "Unknown Error:", err)
     }
   }
 
@@ -87,7 +98,7 @@ router.post("/add-tasks", async (req, res) => {
     const numTasks = await Services.addTasks(queue, tasks, options)
     return res.json({ numTasks })
   } catch (err) {
-    console.log("Unknown Error:", err)
+    customLogger("error", red, "Unknown Error:", err)
     return res.status(500).json({ error: INTERNAL_SERVER_ERROR })
   }
 })
@@ -98,6 +109,7 @@ router.post("/add-tasks", async (req, res) => {
 router.post("/get-next-available-task", async (req, res) => {
   const { queue, type } = req.body
 
+  // VALIDATION
   try {
     if (!queue && !type) {
       throw new QueueError("Either queue or type must be specified")
@@ -108,16 +120,17 @@ router.post("/get-next-available-task", async (req, res) => {
     }
   } catch {
     if (err instanceof ValidationError) {
-      console.log("ValidationError: Invalid queue parameters")
+      customLogger("error", red, "ValidationError: Invalid queue parameters")
       return res.status(400).json({ error: err.message })
     } else if (err instanceof QueueError) {
-      console.log("QueueError")
+      customLogger("error", red, "QueueError")
       return res.status(400).json({ error: err.message })
     } else {
-      console.log("Unknown Error:", err)
+      customLogger("error", red, "Unknown Error:", err)
     }
   }
 
+  // PROCESSING
   try {
     let nextAvailableTask
     if (queue) {
@@ -130,7 +143,7 @@ router.post("/get-next-available-task", async (req, res) => {
       params: nextAvailableTask.params,
     })
   } catch (err) {
-    console.log("Unknown Error:", err)
+    customLogger("error", red, "Unknown Error:", err)
     return res.status(500).json({ error: INTERNAL_SERVER_ERROR })
   }
 })
@@ -141,31 +154,34 @@ router.post("/get-next-available-task", async (req, res) => {
 router.get("/get-results/:queue", async (req, res) => {
   const queue = req.params.queue
 
+  // VALIDATION
+
   try {
     // check queue is valid
     // check queue in db
   } catch (err) {
     if (err instanceof ValidationError) {
-      console.log("ValidationError: Invalid queue parameters")
+      customLogger("error", red, "ValidationError: Invalid queue parameters")
       return res.status(400).json({ error: err.message })
     } else if (err instanceof QueueError) {
-      console.log("QueueError")
+      customLogger("error", red, "QueueError")
       return res.status(400).json({ error: err.message })
     } else {
-      console.log("Unknown Error:", err)
+      customLogger("error", red, "Unknown Error:", err)
     }
   }
 
+  // PROCESSING
   try {
     const response = await Services.getResults(queue)
-    console.log(response)
+    customLogger("error", red, response)
     if (Object.keys(response.results).length === 0) {
       return res.status(400).json({ message: "No completed tasks found" })
     } else {
       return res.status(200).json(response)
     }
   } catch (err) {
-    console.log("Unknown Error:", err)
+    customLogger("error", red, "Unknown Error:", err)
     return res.status(500).json({ error: INTERNAL_SERVER_ERROR })
   }
 })
@@ -176,23 +192,22 @@ router.get("/get-results/:queue", async (req, res) => {
 router.post("/submit-results", async (req, res) => {
   const { id, result, error } = req.body
 
+  // VALIDATION
+  try {
+  } catch {}
+
   try {
     const response = await Services.submitResults({ id, result, error })
-    console.log(response)
+    customLogger("error", red, response)
     if (response) {
-      console.log("All jobs finished")
+      console.log(green("All jobs finished"))
     }
     res.send({ ok: true })
     return
   } catch (err) {
-    console.log(red("Error in route for submit-results: ", err))
-    res.status(500).send({ ok: false, error: "Internal Server Error" })
-    return
+    customLogger("error", red, "Unknown Error:", err)
+    return res.status(500).json({ error: INTERNAL_SERVER_ERROR })
   }
 })
 
 module.exports = router
-
-// add get esults ebndpoiunt tpo dend result
-
-// ma esqkl  file and useeit in iosde
