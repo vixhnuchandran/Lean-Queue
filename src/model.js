55b6da65c139f1f9f4c430a3483524939d721bee
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-18 13:29:28 +0530

    chore: improve database table creation script
    - Refactor table creation script to use 'await pool.connect()' for better connection handling
    - Add logging for the SQL queries before execution for better debugging
    - Minor cleanup and formatting in the table creation module

--- src/model.js
+++ src/model.js
@@ -1,22 +1,25 @@
 const fs = require("fs")
 const path = require("path")
-const pool = require("./db")
+const { pool } = require("./db")
 
 createTables = async () => {
   let client = null
   try {
-    client = pool.connect()
+    client = await pool.connect()
 
     const sqlFilePath = path.join(__dirname, "query.sql")
     const sqlQueries = fs.readFileSync(sqlFilePath, "utf-8")
 
+    console.log("SQL Queries:", sqlQueries)
     await client.query(sqlQueries)
 
     console.log("Tables created successfully")
   } catch (error) {
     console.error("Error creating tables", error)
   } finally {
-    if (client) client.release()
+    if (client) {
+      client.release()
+    }
   }
 }
 
