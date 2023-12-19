0ec2ff2d51849988a48a1dbc3bb3eb7414f524bf
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-19 15:43:45 +0530

    batch size 960 (total 5000 records) test

--- src/services.js
+++ src/services.js
@@ -37,7 +37,7 @@ const addTasks = async (queue, tasks, options, tags) => {
       expiryTime.getTime() + (options?.expiryTime ?? 2 * 60 * 1000) // 2 minutes
     )
 
-    const batchSize = 980
+    const batchSize = 960
     const totalEntries = Object.entries(tasks)
     const totalBatches = Math.ceil(totalEntries.length / batchSize)
 
