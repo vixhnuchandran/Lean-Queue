496efa35eb2e50a7fb49a3ba338149a4c42560b2
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-26 11:37:44 +0530

    chore(debug.js): Refactor and improve query execution with debug mode
    - Refactored the executeQueriesWithDebug function to eliminate redundant code.
    - Enhanced the debug mode to print the debug query result and execution time.

---
+++ src/debug.js
@@ -0,0 +1,23 @@
+const { performance } = require("perf_hooks")
+const executeQueriesWithDebug = async (isDebugMode, debugQuery, mainQuery) => {
+  let startTime, endTime, totalTime, data
+
+  if (isDebugMode) {
+    console.log(await client.query(debugQuery))
+    startTime = performance.now()
+    data = await client.query(mainQuery)
+    endTime = performance.now()
+    totalTime = endTime - startTime
+    console.log("Query execution time", totalTime)
+    return data
+  } else {
+    startTime = performance.now()
+    data = await client.query(mainQuery)
+    endTime = performance.now()
+    totalTime = endTime - startTime
+    console.log("Query execution time", totalTime)
+    return data
+  }
+}
+
+module.exports = { executeQueriesWithDebug }
