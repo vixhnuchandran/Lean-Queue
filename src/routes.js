f7ac25b2fd7fd9c70eee458829c5cf8781859736
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-26 14:39:32 +0530

    10sec delay before sending next task's data

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const routes = require("express").Router()
-const { customLogger, red, delay } = require("./utils")
+const { customLogger, red } = require("./utils")
 const { createQueueAndAddTasks, addTasks } = require("./functions/queue")
 const {
   getNextAvailableTaskByPriority,
@@ -153,8 +153,8 @@ routes.post("/get-next-available-task", async (req, res) => {
         message: "No available task found",
       })
     }
-    await delay(3, 7)
-    console.log(`delay applied`)
+
+    await (ms => new Promise(resolve => setTimeout(resolve, ms)))(10000)()
     return res.status(200).json({
       id: nextAvailableTask.id,
       params: nextAvailableTask.params,
