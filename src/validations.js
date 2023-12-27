f1c72a5c4f21d770ede0046cf016943f2f7f7470
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-27 14:08:48 +0530

    chore: Refactor validation.js
    Refactored the validation.js module to enhance readability and maintainability. Replaced the previous validation functions with new ones and introduced more descriptive error handling. Each validation function now throws specific ValidationError or QueueError for better error identification.
    
    Changes include:
    - Renamed isQueueIdValid to validateQueueId.
    - Updated doesQueueExist to use parameterized queries for improved security.
    - Introduced ValidationError and QueueError classes for specific error types.
    - Refactored isQueueTypeValid to validateQueueType with more descriptive error messages.
    - Updated areOptionsValid to validateOptions with additional checks and improved error messages.
    - Replaced areAllTasksValid with validateTasks, a more comprehensive task validation function.
    - Cleaned up redundant code and improved consistency.

--- src/validations.js
+++ src/validations.js
@@ -1,54 +1,78 @@
-const isQueueIdValid = queueId => {
-  if (typeof queueId === "number" && queueId > 0) {
-    return true
-  } else {
-    return false
-  }
+const { ValidationError, QueueError } = require("./error")
+
+const validateQueueId = async queueId => {
+  if (Number.isInteger(queueId) && queueId <= 0)
+    throw new ValidationError(`invalid queue id`)
+
+  doesQueueExist(queueId)
+
+  return true
 }
 
 const doesQueueExist = async queueId => {
-  const queryStr = `
-  SELECT EXISTS 
-    (SELECT 1 FROM queues WHERE id = ${queueId});
-  `
-  const response = await client.query(queryStr)
-  return response.rows[0].exists
-}
-
-const isQueueTypeValid = type => {
-  if (typeof type === "string" && /^[a-z0-9-]+$/.test(type)) {
-    return true
-  } else {
+  try {
+    const queryStr = `
+      SELECT EXISTS 
+        (SELECT 1 FROM queues WHERE id = $1);
+    `
+    const response = await client.query(queryStr, [queueId])
+    if (!response.rows[0].exists) throw new QueueError(`queue does not exist`)
+  } catch (error) {
+    console.error(`Error in doesQueueExist: ${error.message}`)
     return false
   }
 }
 
-const areOptionsValid = options => {
-  if (typeof options === "object" && typeof options.callback === "string") {
-    const urlRegex = /^(http|https):\/\/[^ "]+$/
-    const isCallbackUrlValid = urlRegex.test(options.callback)
+const validateQueueType = type => {
+  if (typeof type !== "string")
+    throw new ValidationError(`'callbackurl' must be a string type`)
 
-    const expiryTime = options.expiryTime
-    const isExpiryTimeValid = Number.isInteger(expiryTime) && expiryTime > 0
-
-    if (isCallbackUrlValid && isExpiryTimeValid) {
-      return true
-    }
-  }
-  return false
+  if (!/^[a-z0-9-]+$/.test(type))
+    throw new ValidationError(`invalid 'type' format`)
 }
 
-const areAllTasksValid = tasks => {
-  return Object.entries(tasks).every(([taskId, taskParams]) => {
-    return typeof taskParams === "object" && Object.keys(taskParams).length > 0
+const validateOptions = options => {
+  const urlRegex = /^(https?|http):\/\/[^\s/$.?#].[^\s]*$/
+  const expiryTime = options.expiryTime
+
+  if (typeof options !== "object")
+    throw new ValidationError(`'options' must be a object type`)
+
+  if (typeof options.callback !== "string")
+    throw new ValidationError(`'callbackurl' must be a string type`)
+
+  if (!urlRegex.test(options.callback))
+    throw new ValidationError(`invalid 'callbackurl' format`)
+
+  if (!Number.isInteger(expiryTime) && expiryTime <= 0)
+    throw new ValidationError(`invalid 'expiryTime'`)
+
+  return true
+}
+
+const validateTasks = tasks => {
+  tasks.forEach((task, i) => {
+    if (typeof task !== "object")
+      throw new ValidationError(`tasks[${i}]: not an object`)
+
+    if (task.taskId === undefined || task.taskId === null)
+      throw new ValidationError(`tasks[${i}]: taskId missing`)
+
+    if (task.params === undefined || task.params === null)
+      throw new ValidationError(`tasks[${i}]: params missing`)
+
+    if (task.priority !== undefined && task.priority !== null) {
+      if (!Number.isInteger(task.priority))
+        throw new ValidationError(`tasks[${i}]: priority not an integer`)
+    }
+
+    return true
   })
 }
 
 module.exports = {
-  doesQueueExist,
-  isQueueIdValid,
-  isQueueTypeValid,
-  areOptionsValid,
-  areAllTasksValid,
-  doesQueueExist,
+  validateQueueType,
+  validateQueueId,
+  validateOptions,
+  validateTasks,
 }
