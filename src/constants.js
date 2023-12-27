8e1c1bcd0cb81097a4ab97b9bc3a68eb407be991
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-27 14:14:54 +0530

    chore: Add constants file
    Added a new constants.js file with predefined constants:
    - INTERNAL_SERVER_ERROR
    - HTTP_OK
    - HTTP_BAD_REQUEST
    - HTTP_INTERNAL_SERVER_ERROR.

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const INTERNAL_SERVER_ERROR = "Internal server error"
+const HTTP_OK = 200
+const HTTP_BAD_REQUEST = 400
+const HTTP_INTERNAL_SERVER_ERROR = 500
+
+module.exports = {
+  INTERNAL_SERVER_ERROR,
+  HTTP_OK,
+  HTTP_BAD_REQUEST,
+  HTTP_INTERNAL_SERVER_ERROR,
+}
