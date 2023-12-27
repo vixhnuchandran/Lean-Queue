37746ff1d72a62901468013e86f12bd7d9deac74
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-27 14:12:58 +0530

    chore: Refactor validation.js
    Refactored the validation.js module to enhance readability and maintainability. Replaced the previous validation functions with new ones and introduced more descriptive error handling. Each validation function now throws specific ValidationError or QueueError for better error identification.
    
    Changes include:
    - Renamed isQueueIdValid to validateQueueId.
    - Updated doesQueueExist to use parameterized queries for improved security.
    - Introduced ValidationError and QueueError classes for specific error types.
    - Refactored isQueueTypeValid to validateQueueType with more descriptive error messages.
    - Updated areOptionsValid to validateOptions with additional checks and improved error messages.
    - Replaced areAllTasksValid with validateTasks, a more comprehensive task validation function.
    - Cleaned up redundant code and improved consistency.

--- src/error.js
+++ src/error.js
@@ -1,3 +1,11 @@
+const { customLogger, red } = require("./utils")
+const {
+  INTERNAL_SERVER_ERROR,
+  HTTP_OK,
+  HTTP_BAD_REQUEST,
+  HTTP_INTERNAL_SERVER_ERROR,
+} = require("./constants")
+
 class QueueError extends Error {
   constructor(message) {
     super(message)
@@ -14,4 +22,35 @@ class ValidationError extends Error {
   }
 }
 
-module.exports = { QueueError, ValidationError }
+const handleCustomError = (err, res) => {
+  if (err instanceof ValidationError || err instanceof QueueError) {
+    customLogger("error", red, `${err.constructor.name}: ${err.message}`)
+    return res.status(HTTP_BAD_REQUEST).json({ error: err.message })
+  }
+
+  customLogger("error", red, `error: ${err.message}\n${err.stack}`)
+  return res
+    .status(HTTP_INTERNAL_SERVER_ERROR)
+    .json({ error: INTERNAL_SERVER_ERROR })
+}
+
+const handleUnknownError = (err, res) => {
+  customLogger("error", red, `error: ${err.message}\n${err.stack}`)
+  return res
+    .status(HTTP_INTERNAL_SERVER_ERROR)
+    .json({ error: INTERNAL_SERVER_ERROR })
+}
+
+const handleAppErrors = (err, res) => {
+  if (err instanceof ValidationError || err instanceof QueueError) {
+    handleCustomError(err, res)
+  } else {
+    handleUnknownError(err, res)
+  }
+}
+
+module.exports = {
+  QueueError,
+  ValidationError,
+  handleAppErrors,
+}
