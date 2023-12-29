c56ffbbcd2fac78d993f04d099008116cce67354
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-29 13:57:11 +0530

    refactor: Improve middleware structure and error handling in app
    - Reorganized middleware for better readability and maintenance.
    - Introduced explicit middleware for request ID attachment and query manager handling.
    - Enhanced error handling with a dedicated middleware for consistent error responses.
    - Streamlined server initialization and added clearer console logs.

--- src/app.js
+++ src/app.js
@@ -1,43 +1,33 @@
 const express = require("express")
 const routes = require("./routes")
-const { pool } = require("./db")
-require("dotenv").config()
+const { HTTP_OK } = require("./constants")
 const {
-  INTERNAL_SERVER_ERROR,
-  HTTP_OK,
-  HTTP_BAD_REQUEST,
-  HTTP_INTERNAL_SERVER_ERROR,
-} = require("./constants")
+  attachRequestIdMiddleware,
+  attachQueryManagerMiddleware,
+  releaseQueryManagerClientMiddleware,
+  errorHandlingMiddleware,
+} = require("./middleware")
+require("dotenv").config()
 
 const app = express()
 
-// middleware
+// middlewares
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json({ limit: "50mb" }))
-app.use(async (req, res, next) => {
-  const client = await pool.connect()
-  req.dbClient = client
-  next()
-})
+
+app.use(attachRequestIdMiddleware)
+
+app.use(attachQueryManagerMiddleware)
 
 // routes
 app.get("/", (req, res) => {
   res.sendStatus(HTTP_OK)
 })
 app.use("/", routes)
-app.get("/*", (req, res) => {
-  res.sendStatus(HTTP_BAD_REQUEST)
-})
 
-// error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(HTTP_INTERNAL_SERVER_ERROR).send({ error: INTERNAL_SERVER_ERROR })
-  if (req.dbClient) {
-    req.dbClient.release()
-    next()
-  }
-})
+app.use(errorHandlingMiddleware)
+
+app.use(releaseQueryManagerClientMiddleware)
 
 const PORT = process.env.PORT || 8383
 app.listen(PORT, () => {
